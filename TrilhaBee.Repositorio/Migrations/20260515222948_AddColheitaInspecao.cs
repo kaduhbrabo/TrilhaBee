091116f9c16b9bad708d739656c56c0ccf1e0598
carlos.epmoreira <carlos.epmoreira@outlook.com>
2026-05-18 21:04:48 -0300

    feat(back): adicionar migration para colheita na inspecao

---
+++ TrilhaBee.Repositorio/Migrations/20260515222948_AddColheitaInspecao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrilhaBee.Repositorio.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColheitaInspecao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataColheita",
+                table: "Inspecao",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MelColetado",
+                table: "Inspecao",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataColheita",
+                table: "Inspecao");
+
+            migrationBuilder.DropColumn(
+                name: "MelColetado",
+                table: "Inspecao");
+        }
+    }
+}
